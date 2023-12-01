99edc2cf2b409d8509fd4058e53aa0a446e8a830
santiago hurtado <sahurtaro@gmail.com>
2023-12-01 12:53:49 -0500

    164. Sorting arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -186,6 +189,13 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -378,3 +388,19 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+
+//Sort strings
+const owners = ['Jonas', 'Jack', 'Adam', 'Martha  '];
+console.log(owners.sort());
+
+//Sort numbers
+console.log(movements);
+//return <0; A,B (keep order)
+//return >0; B,A (switch order)
+movements.sort((a, b) => {
+  if (a > b) 1;
+  if (b > a) -1;
+});
+
+//Resumido
+movements.sort((a, b) => a - b);
